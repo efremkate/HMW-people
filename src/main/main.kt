98c510642dd20f12dbf41bea934a2b993070cd3a
Ekaterina Efremova <katerina.usatykh@gmail.com>
2022-09-04 22:16:02 +0300

    edit division

--- src/main/main.kt
+++ src/main/main.kt
@@ -5,7 +5,7 @@ fun main() {
     val naming1 = "человеку"
     val naming2 = "людям"
 
-    val result = if (likes % 10 != 1) naming2 else naming1
+    val result = if (likes / 10 != 1) naming2 else naming1
 
     println("Понравилось $likes $result")
 }
