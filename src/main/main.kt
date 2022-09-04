b23f025ba6aa59f2ea888adf6df4886ebf04ea44
Ekaterina Efremova <katerina.usatykh@gmail.com>
2022-09-04 22:16:02 +0300

    edit formula

--- src/main/main.kt
+++ src/main/main.kt
@@ -1,11 +1,11 @@
 package ru.netology
 
 fun main() {
-    val likes = 1000
+    val likes = 121
     val naming1 = "человеку"
     val naming2 = "людям"
 
-    val result = if (likes % 10 != 1) naming2 else naming1
+    val result = if (likes > 10 && likes < 21 || likes % 100 == 11) naming2 else naming1
 
     println("Понравилось $likes $result")
 }
